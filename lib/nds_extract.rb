c869f02f50b741069c583f347b5b97a1778b03bf
Ha Nguyen <nkei1908@yahoo.com>
2020-01-14 00:10:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,7 +48,12 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-  
+  index = 0 
+  while movies_collection[index] do
+    movies_collection[index][:name] = name
+    index += 1
+  end  
+  movies_collection
 end
 
 
