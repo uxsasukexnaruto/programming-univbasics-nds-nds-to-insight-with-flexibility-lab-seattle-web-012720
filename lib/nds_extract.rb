b7b7edf15313e74b02d1846f354f93a16437e67f
Ha Nguyen <nkei1908@yahoo.com>
2020-01-14 00:36:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -94,7 +94,10 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  set = [[]]
   
+  
+  return set 
 end
 
 # ----------------    End of Your Code Region --------------------
